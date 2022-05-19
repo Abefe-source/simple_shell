828c82ff9b26b4b397a3c98a6a74c0e6165ab4cc
Abefe-source <oladegaadeola@gmail.com>
2022-05-18 22:41:29 -0700

    fixing updates

--- built_in.c
+++ built_in.c
@@ -1,47 +1,28 @@
 #include "main.h"
 
 /**
- *builtin- it checks if the user input is a built-in command and executes it
- *@env: specifies the current environment
- *@argv: specifies the list of arguments provided
- *@line: specifies the line from getline
- *@extstat: status for empty exit
- *Returns 1 if builtin and 0 if failure
+ * get_builtin - builtin that pais the command in the arg
+ * @cmd: command
+ * Return: function pointer of the builtin command
  */
-int builtin(char **env, char **argv, char *line, int extstat)
+int (*get_builtin(char *cmd))(data_shell *)
 {
+	builtin_t builtin[] = {
+		{ "env", _env },
+		{ "exit", exit_shell },
+		{ "setenv", _setenv },
+		{ "unsetenv", _unsetenv },
+		{ "cd", cd_shell },
+		{ "help", get_help },
+		{ NULL, NULL }
+	};
 	int i;
 
-	if (_strcmp("env", argv[0]) == 0)/*envronment*/
+	for (i = 0; builtin[i].name; i++)
 	{
-		if (argv[1] == NULL)
-		{
-			for (i = 0 ; env[i] ; i++)
-			{
-				write(1, env[i], _strlen(env[i]));
-				write(1, "\n", 1);
-			}
-			free_shell(argv, line);
-		}
-		else
-		{
-			write(STDERR_FILENO, "env: ", 5);
-			write(STDERR_FILENO, argv[1], strlen(argv[0]) + 1);
-			write(STDERR_FILENO, ": No such file or directory\n",
-			      28);
-		}
-		return (1);
+		if (_strcmp(builtin[i].name, cmd) == 0)
+			break;
 	}
-	else if (_strcmp("exit", argv[0]) == 0)
-	{
-		if (argv[1] == NULL)
-		{
-			free_shell(argv, line);
-			exit(extstat);
-		}
-		i = atoi(argv[1]);
-		free_shell(argv, line);
-		exit(i);
-	}
-	return (0);
+
+	return (builtin[i].f);
 }
