fac8ef08386bdcdf5152eb25aabfbbe897d5d19b
joyodeyemi <joybella4eva@gmail.com>
2022-05-19 09:44:04 +0100

    fixed error

--- main.h
+++ main.h
@@ -92,39 +92,39 @@ typedef struct builtin_s
 	int (*f)(data_shell *datash);
 } builtin_t;
 
-/* aux_lists.c */
+/* auxLists.c */
 sep_list *add_sep_node_end(sep_list **head, char sep);
 void free_sep_list(sep_list **head);
 line_list *add_line_node_end(line_list **head, char *line);
 void free_line_list(line_list **head);
 
-/* aux_lists2.c */
+/* auxLists2.c */
 r_var *add_rvar_node(r_var **head, int lvar, char *var, int lval);
 void free_rvar_list(r_var **head);
 
-/* aux_str functions */
+/* auxStr.c */
 char *_strcat(char *dest, const char *src);
 char *_strcpy(char *dest, char *src);
 int _strcmp(char *s1, char *s2);
 char *_strchr(char *s, char c);
 int _strspn(char *s, char *accept);
 
-/* aux_mem.c */
+/* auxMem.c */
 void _memcpy(void *newptr, const void *ptr, unsigned int size);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 char **_reallocdp(char **ptr, unsigned int old_size, unsigned int new_size);
 
-/* aux_str2.c */
+/* auxStr2.c */
 char *_strdup(const char *s);
 int _strlen(const char *s);
 int cmp_chars(char str[], const char *delim);
 char *_strtok(char str[], const char *delim);
 int _isdigit(const char *s);
 
-/* aux_str3.c */
+/* auxStr3.c */
 void rev_string(char *s);
 
-/* check_syntax_error.c */
+/* check_syntaxErrors.c */
 int repeated_char(char *input, int i);
 int error_sep_op(char *input, int i, char last);
 int first_char(char *input, int *i);
@@ -138,7 +138,7 @@ void shell_loop(data_shell *datash);
 /* read_line.c */
 char *read_line(int *i_eof);
 
-/* split.c */
+/* _split.c */
 char *swap_char(char *input, int bool);
 void add_nodes(sep_list **head_s, line_list **head_l, char *input);
 void go_next(sep_list **list_s, line_list **list_l, data_shell *datash);
@@ -165,17 +165,17 @@ int is_executable(data_shell *datash);
 int check_error_cmd(char *dir, data_shell *datash);
 int cmd_exec(data_shell *datash);
 
-/* env1.c */
+/* env_1.c */
 char *_getenv(const char *name, char **_environ);
 int _env(data_shell *datash);
 
-/* env2.c */
+/* env_2.c */
 char *copy_info(char *name, char *value);
 void set_env(char *name, char *value, data_shell *datash);
 int _setenv(data_shell *datash);
 int _unsetenv(data_shell *datash);
 
-/* cd.c */
+/* _cd.c */
 void cd_dot(data_shell *datash);
 void cd_to(data_shell *datash);
 void cd_previous(data_shell *datash);
@@ -184,7 +184,7 @@ void cd_to_home(data_shell *datash);
 /* cd_shell.c */
 int cd_shell(data_shell *datash);
 
-/* get_builtin */
+/* get_builtin.c */
 int (*get_builtin(char *cmd))(data_shell *datash);
 
 /* _exit.c */
@@ -195,13 +195,13 @@ int get_len(int n);
 char *aux_itoa(int n);
 int _atoi(char *s);
 
-/* aux_error1.c */
+/* auxError1.c */
 char *strcat_cd(data_shell *, char *, char *, char *);
 char *error_get_cd(data_shell *datash);
 char *error_not_found(data_shell *datash);
 char *error_exit_shell(data_shell *datash);
 
-/* aux_error2.c */
+/* auxError2.c */
 char *error_get_alias(char **args);
 char *error_env(data_shell *datash);
 char *error_syntax(char **args);
@@ -209,25 +209,25 @@ char *error_permission(char **args);
 char *error_path_126(data_shell *datash);
 
 
-/* get_error.c */
+/* getError.c */
 int get_error(data_shell *datash, int eval);
 
-/* get_sigint.c */
+/* getSigint.c */
 void get_sigint(int sig);
 
-/* aux_help.c */
+/* auxHelp.c */
 void aux_help_env(void);
 void aux_help_setenv(void);
 void aux_help_unsetenv(void);
 void aux_help_general(void);
 void aux_help_exit(void);
 
-/* aux_help2.c */
+/* auxHelp2.c */
 void aux_help(void);
 void aux_help_alias(void);
 void aux_help_cd(void);
 
-/* get_help.c */
+/* getHelp.c */
 int get_help(data_shell *datash);
 
 #endif
