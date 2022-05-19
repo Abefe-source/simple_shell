d5c63a50540abab32fc977c542974cbd9a7c3fc4
Abefe-source <oladegaadeola@gmail.com>
2022-05-18 21:16:19 -0700

    update on files

--- main.c
+++ main.c
@@ -1,67 +1,73 @@
 #include "main.h"
 
 /**
- *main- it runs a simple shell
- * @ac: the variable is meant to be possibly unused
- * @av: the variable is meant to be possibly unused
- * @env: indicates the environment variable
- *Returns 0 if successful and -1 on failure
+ * free_data - frees data structure
+ *
+ * @datash: data structure
+ * Return: no return
  */
-int main(int ac, char **av, char **env)
+void free_data(data_shell *datash)
 {
-	char **argv = NULL, *line = NULL;
-	pid_t child;
-	ssize_t characters = 0;
-	size_t size = 0;
-	size_t i;
-	int status = 1, extstat = 0;
-	(void)ac;
-	(void)av;
+	unsigned int i;
 
-	signal(SIGINT, sighelp);
-
-	for (i = 1; characters != -1; i++)
+	for (i = 0; datash->_environ[i]; i++)
 	{
-		size = 0;
-		characters = -1;
-		prompt(1);
-		characters = getline(&line, &size, stdin);
-		fflush(stdin);		/*this gets the command in line*/
-		if (characters == -1)
-		{
-			getline_fail(argv, line);
-			break;
-		}
-		argv = tok(line, "\t\n ");		/*this runs the tok func on line*/
-		if (argv == NULL)
-		{
-			free(line);
-			line = NULL;
-			continue;
-		}
-		if (builtin(env, argv, line, extstat) == 1)/*variable is meant to be possibly unused*/
-			continue;
-		argv = _path(1, argv, env);		/*checks/append path*/
-		child = fork();		/*here, a child is created*/
-		if (child == -1)
-		{
-			free_shell(argv, line);
-			return (-1);
-		}
-		if (child == 0)
-		{
-			stat_exec(argv, line, i, env);		/*the command is executed*/
-			_exit(1);
-		}
-		else
-		{
-			free_shell(argv, line);
-			wait(&status);		/*here it waits for current process to finish*/
-			if (WIFEXITED(status))
-				extstat = WEXITSTATUS(status);
-		}
+		free(datash->_environ[i]);
 	}
-	if (isatty(0))
-		free_shell(argv, line);		/*frees all in parent*/
-	return (0);
+
+	free(datash->_environ);
+	free(datash->pid);
+}
+
+/**
+ * set_data - Initialize data structure
+ *
+ * @datash: data structure
+ * @av: argument vector
+ * Return: no return
+ */
+void set_data(data_shell *datash, char **av)
+{
+	unsigned int i;
+
+	datash->av = av;
+	datash->input = NULL;
+	datash->args = NULL;
+	datash->status = 0;
+	datash->counter = 1;
+
+	for (i = 0; environ[i]; i++)
+		;
+
+	datash->_environ = malloc(sizeof(char *) * (i + 1));
+
+	for (i = 0; environ[i]; i++)
+	{
+		datash->_environ[i] = _strdup(environ[i]);
+	}
+
+	datash->_environ[i] = NULL;
+	datash->pid = aux_itoa(getpid());
+}
+
+/**
+ * main - Entry point
+ *
+ * @ac: argument count
+ * @av: argument vector
+ *
+ * Return: 0 on success.
+ */
+int main(int ac, char **av)
+{
+	data_shell datash;
+	(void) ac;
+
+	signal(SIGINT, get_sigint);
+	set_data(&datash, av);
+	shell_loop(&datash);
+	free_data(&datash);
+	if (datash.status < 0)
+		return (255);
+	return (datash.status);
 }
