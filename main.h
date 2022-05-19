6f0b144aa8a030e0f42fb856ab51071aafcab5e9
joyodeyemi <joybella4eva@gmail.com>
2022-05-19 02:23:06 +0100

    fixed decleration errors in main.h

--- main.h
+++ main.h
@@ -19,6 +19,7 @@ void free_shell(char **argv, char *line);
 /*stat_exec.c*/
 int stat_exec(char **argv, char *line, size_t i, char **env);
 int _strlen(char *);
+int print_number(size_t a);
 
 /*path.c*/
 char **_path(int argc, char **argv, char **env);
@@ -27,9 +28,12 @@ char *_strdup(char *str);
 
 /*built_in.c*/
 int builtin(char **env, char **argv, char *line, int extstat);
+int _strcmp(const char *s1, char *s2);
 
 /*main.c*/
 void getline_fail(char **argv, char *line);
 char **tok(char *st, char *dil);
+void sighelp(int a);
+void prompt(int a);
 
 #endif
