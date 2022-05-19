3a6915dc8e8c139bc4158310c8a88eaeb84d426f
joyodeyemi <joybella4eva@gmail.com>
2022-05-19 02:13:10 +0100

    fixed decleration error

--- main.h
+++ main.h
@@ -12,13 +12,24 @@
 
 /*env.c*/
 char *_getenv(const char *name, char **env);
+
 /*free_shell*/
 void free_shell(char **argv, char *line);
+
 /*stat_exec.c*/
 int stat_exec(char **argv, char *line, size_t i, char **env);
+int _strlen(char *)
+
 /*path.c*/
 char **_path(int argc, char **argv, char **env);
+char *str_concat(char *s1, char *s2);
+char *_strdup(char *str);
+
 /*built_in.c*/
 int builtin(char **env, char **argv, char *line, int extstat);
+
+/*main.c*/
+void getline_fail(char **argv, char *line);
+char **tok(char *st, char *dil);
 
 #endif
